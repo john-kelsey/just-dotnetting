a05b36b6a88b28d79bb06ee25fcf9174330f86ad
John Kelsey <joeadamson2009151@gmail.com>
2024-04-16 16:50:47 +0300

    Just learning C#

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name} , on {currentDate:d}");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
+
